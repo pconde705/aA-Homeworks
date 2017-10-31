b53eb831f3d1fc38db985c3eaca71a335270c785
Patrick Conde <patrick.conde.costas@gmail.com>
2017-10-30 22:20:13 -0700

    Finished homework

--- W5D2/Polymorphism and Concerns/app/models/corgi.rb
+++ W5D2/Polymorphism and Concerns/app/models/corgi.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: corgis
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Corgi < ApplicationRecord
+
+  include Toyable
 end
