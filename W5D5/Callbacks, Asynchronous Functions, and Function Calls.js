834156292dbc08cbafa99b516db0e70d59c99394
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-02 23:20:04 -0700

    Finished homework

--- W5D5/Callbacks, Asynchronous Functions, and Function Calls.js
+++ W5D5/Callbacks, Asynchronous Functions, and Function Calls.js
@@ -30,3 +30,27 @@ reader.question("Would you like some tea?", function (answerTea) {
   reader.close();
   });
 });
+
+
+
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
+
+Noodles.chase(Markov)
+
+Markov.chase.apply(Markov, [Noodles]);
+Noodles.chase.call(Markov, Noodles);
